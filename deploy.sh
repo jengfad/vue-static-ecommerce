a566c254718268a49359ffe85c4afd1319535b7e
Jennifer Fadriquela <jenniferfadriquela@gmail.com>
2020-10-11 14:10:08 +0800

    test deploy

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:jengfad/vue-static-ecommerce.git master:gh-pages
+git push -f https://github.com/jengfad/vue-static-ecommerce.git master:gh-pages
 cd -
