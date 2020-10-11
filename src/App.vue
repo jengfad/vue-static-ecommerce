0cd6727c572162186f208eb58e706d8ee19e52bb
Jennifer Fadriquela <jenniferfadriquela@gmail.com>
2020-10-11 17:56:28 +0800

    add painting image

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,24 @@
           <div class="flip-card-inner">
               <div class="flip-card-front mb-5">
                   <div class="product-preview flex-column flex-md-row d-flex">
-                      <div class="product-info p-2 p-md-5 col-md-6">
-                          <h1 class="mb-5">
-                          <strong>My paintings shop!</strong>
-                          </h1>
-                          <div class="product-detail-info">
-                              <div class="input-group mb-3">
-                                  <div class="input-group-prepend">
-                                      <label class="input-group-text" for="inputGroupSelect01">Painting</label>
-                                  </div>
-                                  <PaintingsSelectbox />
-                              </div>
-                              <PaintingDescription />
-                              <div id="add-to-cart-form-view"></div>
-                          </div>
-                      </div>
+                        <div class="product-info p-2 p-md-5 col-md-6">
+                            <h1 class="mb-5">
+                            <strong>My paintings shop!</strong>
+                            </h1>
+                            <div class="product-detail-info">
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <label class="input-group-text" for="inputGroupSelect01">Painting</label>
+                                    </div>
+                                    <PaintingsSelectbox />
+                                </div>
+                                <PaintingDescription />
+                                <div id="add-to-cart-form-view"></div>
+                            </div>
+                        </div>
+                        <div class="product-images p-2 p-md-5 col-md-6 bg-light align-items-center d-flex">
+                            <PaintingImage />
+                        </div>
                   </div>
               </div>
           </div>
@@ -30,12 +33,14 @@
 <script>
 import PaintingsSelectbox from './components/paintings/PaintingsSelectbox.vue';
 import PaintingDescription from './components/paintings/PaintingDescription.vue';
+import PaintingImage from './components/paintings/PaintingImage.vue';
 
 export default {
   name: 'App',
   components: {
     PaintingsSelectbox,
     PaintingDescription,
+    PaintingImage,
   },
 }
 </script>
---
+++ src/components/paintings/PaintingImage.vue
@@ -0,0 +1,17 @@
+<template>
+    <img :src="selectedPainting.imageUrl" alt="" class="mw-100 shadow">
+</template>
+
+<script>
+
+import paintings from '../../data/static';
+
+export default {
+    computed: {
+        selectedPainting() {
+            return paintings[this.$store.state.paintings.selectedPainting];
+        }
+    },
+}
+
+</script>
